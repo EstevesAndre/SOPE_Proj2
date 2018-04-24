2c0776c4700b076ed88846610c3996c05f17f27c
luisdiogo98 <luisdiogo98@hotmail.com>
2018-04-24 15:04:55 +0100

    Added constants file

---
+++ constants.h
@@ -0,0 +1,12 @@
+#define MAX_ROOM_SEATS 9999
+#define MAX_CLI_SEATS 99
+#define WIDTH_PID 4
+#define WIDTH_XXNN 4
+
+#define REQ_SUCCESSFUL 0
+#define REQ_ERR_OVER_MAX_SEATS -1
+#define REQ_ERR_UNDER_SEAT_ID -2
+#define REQ_ERR_SEAT_ID_INV -3
+#define REQ_ERR_PARAM_OTHER -4
+#define REQ_ERR_UNNAV_SEAT -5
+#define REQ_ERR_ROOM_FULL -6
